3759166fdddb814169f5efa05eed049502dda5df
Winfried Zeißig <zeissigw@gmx.de>
2026-03-18 14:03:14 +0100

    Add simple ESP32 webserver

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,8 +1,22 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WebServer.h>
 
-const char* ssid = "DEIN_WLAN_NAME";
-const char* password = "DEIN_WLAN_PASSWORT";
+const char* ssid = "FRITZ!Box 7530 MI-2G&5G";
+const char* password = "72335155615336606495";
+
+WebServer server(80);
+
+void handleRoot() {
+    Serial.println("Browser Anfrage empfangen!");
+    server.send(200, "text/plain", "ESP32 Server funktioniert!");
+}
+
+void handleNotFound() {
+    Serial.print("Unbekannte Anfrage: ");
+    Serial.println(server.uri());
+    server.send(404, "text/plain", "Seite nicht gefunden");
+}
 
 void setup() {
     Serial.begin(115200);
@@ -16,11 +30,18 @@ void setup() {
         Serial.print(".");
     }
 
-    Serial.println("");
+    Serial.println();
     Serial.println("WLAN verbunden!");
     Serial.print("IP-Adresse: ");
     Serial.println(WiFi.localIP());
+
+    server.on("/", handleRoot);
+    server.onNotFound(handleNotFound);
+    server.begin();
+
+    Serial.println("Webserver gestartet!");
 }
 
 void loop() {
+    server.handleClient();
 }
